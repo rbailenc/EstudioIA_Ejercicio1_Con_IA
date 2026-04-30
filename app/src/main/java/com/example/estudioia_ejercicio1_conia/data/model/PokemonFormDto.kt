7c3e6c4e832edbfa4289747c99292328b9c2c541
ruser215 <www.ruser215@gmail.com>
2026-04-30 10:42:33 +0200

    Agregación del readme

---
+++ app/src/main/java/com/example/estudioia_ejercicio1_conia/data/model/PokemonFormDto.kt
@@ -0,0 +1,8 @@
+package com.example.estudioia_ejercicio1_conia.data.model
+
+import com.google.gson.annotations.SerializedName
+
+data class PokemonFormDto(
+    val sprites: SpritesDto
+)
+
---
+++ app/src/main/java/com/example/estudioia_ejercicio1_conia/domain/model/Pokemon.kt
@@ -0,0 +1,8 @@
+package com.example.estudioia_ejercicio1_conia.domain.model
+
+data class Pokemon(
+    val id: Int,
+    val name: String,
+    val url: String,
+    val imageUrl: String?
+)
